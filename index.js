25b8502b0e26de04106429f9b60d5ff77d5bbbe0
Lizzette <Lizzetter00@gmail.com>
2021-05-01 23:22:42 -0400

    commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+//  function saturdayFun(){
+    //  return String = 'This Saturday, I want to go eat'
+//  }
+// console.log(String);
+
+function wrapAdjectiv(special){
+    const special = ('Great','Smart','Funny');
+    String = 'You are Funny';
+    return function(){
+    console.log('You are'(special));
+    }
+
+}
+
